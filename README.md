b5ed676149771ef5f96a2f87322c1d4d96872efb
Hydrazer <explodingaerosal@gmail.com>
2022-09-25 19:41:58 -0600

    added wasm build instructions

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 haskellHop
 
-inspiration from tsoding (betterttv emote) and andriamanitra (haskelHop haskellHop)
+inspiration from tsoding (betterttv emote) and andriamanitra (haskelHop haskellHop) and belzile (rust wasm repo)
+
+## Build for the Web
+### Prerequisites
+
+```sh
+rustup target add wasm32-unknown-unknown
+cargo install -f wasm-bindgen-cli
+```
+
+```sh
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/haskell_hop.wasm
+npx serve .
+```
--- html.bash
+++ html.bash
@@ -1,6 +1,8 @@
 '/mnt/d/Program Files/PowerShell/7/pwsh.exe' 'D:\\Program Files\\PowerShell\\7\\bevy_wasm.ps1'
-mv out/haskell_hop.js out/bruh.js
-echo '
+# cargo build --release --target wasm32-unknown-unknown
+# wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/haskell_hop.wasm
+
+s=$(echo '
 const observer = new MutationObserver((mutations, obs) => {
   var canvas = document.getElementsByTagName("canvas")[0];
   if (canvas) {
@@ -15,4 +17,7 @@ observer.observe(document, {
   childList: true,
   subtree: true
 });
-' | cat - 'out/bruh.js' > 'out/haskell_hop.js'
+' | cat - 'out/haskell_hop.js')
+
+
+echo "$s" > 'out/haskell_hop.js'
